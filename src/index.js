2d6de75382528e15a5d09c25c54428b9abb76c59
Jordan~ <jordan.brewer509@gmail.com>
2022-12-19 17:46:13 -0800

    finished challenge

--- src/index.js
+++ src/index.js
@@ -40,3 +40,57 @@ const siteContent = { // DO NOT CHANGE THIS OBJECT
 };
 
 console.log('project wired!')
+
+const aSelector = document.querySelectorAll('a');
+
+aSelector[0].textContent = "Services";
+aSelector[1].textContent = "Product";
+aSelector[2].textContent = "Vision";
+aSelector[3].textContent = "Features";
+aSelector[4].textContent = "About";
+aSelector[5].textContent = "Contact";
+
+aSelector[6].textContent = "Copyright Great Idea! 2021"
+aSelector.forEach(a => a.setAttribute('class', 'italic'));
+aSelector[6].setAttribute('class', 'bold');
+
+const logoImgSelector = document.querySelector('#logo-img');
+const ctaImgSelector = document.querySelector('#cta-img');
+const accentImgSelector = document.querySelector('#middle-img');
+
+logoImgSelector.setAttribute('src', 'http://localhost:9000/img/logo.png');
+ctaImgSelector.setAttribute('src', 'http://localhost:9000/img/cta.png');
+accentImgSelector.setAttribute('src', 'http://localhost:9000/img/accent.png');
+
+const h1Selector = document.querySelector('h1');
+
+h1Selector.textContent = "DOM Is Awesome";
+
+const buttonSelector = document.querySelector('button');
+
+buttonSelector.textContent = "Get Started";
+
+const h4Selector = document.querySelectorAll('h4');
+
+h4Selector[0].textContent = "Features";
+h4Selector[1].textContent = "About";
+h4Selector[2].textContent = "Services";
+h4Selector[3].textContent = "Product";
+h4Selector[4].textContent = "Vision";
+
+h4Selector[5].textContent = "Contact";
+
+const pSelector = document.querySelectorAll('p');
+
+pSelector[0].textContent = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+pSelector[1].textContent = "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+pSelector[2].textContent = "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+pSelector[3].textContent = "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+pSelector[4].textContent = "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+
+
+pSelector[5].textContent = "123 Way 456 Street Somewhere, USA";
+pSelector[6].textContent = "1 (888) 888-8888";
+pSelector[7].textContent = "sales@greatidea.io";
+
+
